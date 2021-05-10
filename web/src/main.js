d97aece7af1fd66b904af34f7474cd2febe01bfa
Olivier Meunier <olivier@neokraft.net>
2021-05-10 20:54:52 +0200

    Add a "js" class to the body element.
    Instead of doing it in the script bundle, we do it right after the
    body element appeared. It lets us style things when js is supported
    or when it's not.

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,6 @@
 import { Application } from "stimulus"
 import { definitions } from "stimulus:./controllers"
 
-document.body.classList.add("js")
-
 const application = Application.start()
 application.load(definitions)
 

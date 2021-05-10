8e0757546136c2e499c8754b3fe980c446e8b2da
Olivier Meunier <olivier@neokraft.net>
2021-05-10 20:36:58 +0200

    Refactored reveal stimulus controller.
    Use only "content" targets and a controller attribute "visible" to
    show or hide the content.
    
    This gives a css selector we can use, should we need to.

--- web/src/controllers/reveal_controller.js
+++ web/src/controllers/reveal_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static get targets () {
-    return ["trigger", "hide", "show"]
+    return ["trigger", "content"]
   }
   static get values () {
     return {
@@ -11,29 +11,24 @@ export default class extends Controller {
   }
 
   connect () {
-    this.toggle()
     this.triggerTargets.forEach(e => {
       e.style.cursor = "pointer"
       e.setAttribute("data-action", `click->${this.identifier}#toggle`)
     })
   }
 
+  visibleValueChanged() {
+    if (this.visibleValue) {
+      this.contentTargets.forEach(e => e.style.display = "")
+    } else {
+      this.contentTargets.forEach(e => e.style.display = "none")
+    }
+  }
+
   toggle(evt) {
     if (evt) {
       evt.preventDefault()
     }
-    this.visibleValue ? this.show() : this.hide()
-  }
-
-  show() {
-    this.hideTargets.forEach(e => e.style.display = "")
-    this.showTargets.forEach(e => e.style.display = "none")
-    this.visibleValue = false
-  }
-
-  hide () {
-    this.hideTargets.forEach(e => e.style.display = "none")
-    this.showTargets.forEach(e => e.style.display = "")
-    this.visibleValue = true
+    this.visibleValue = !this.visibleValue
   }
 }

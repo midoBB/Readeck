6d9e1b9a88987b5b94aedf4cafe7bf28b2fe7f14
Olivier Meunier <olivier@neokraft.net>
2021-09-12 22:07:27 +0200

    Added pkg/superbus
    Superbus is a very simple implementation of an event listener with only
    two backends: in-memory and redis.

---
+++ pkg/superbus/store.go
@@ -0,0 +1,104 @@
+package superbus
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Store is a very basic key/value store.
+type Store interface {
+	Get(string) string
+	Set(string, string, time.Duration) error
+	Del(string) error
+}
+
+// RedisStore implements KvStore with redis.
+type RedisStore struct {
+	rdb    *redis.Client
+	prefix string
+}
+
+// NewRedisStore returns a RedisStore instance. The prefix is used for each
+// key operation.
+func NewRedisStore(rdb *redis.Client, prefix string) *RedisStore {
+	return &RedisStore{
+		rdb:    rdb,
+		prefix: prefix,
+	}
+}
+
+// key returns a keys with the given namespace prefix.
+func (s *RedisStore) key(key string) string {
+	return fmt.Sprintf("%s:%s", s.prefix, key)
+}
+
+// Get returns a value for the given key. Returns an empty string when the
+// value does not exist.
+func (s *RedisStore) Get(key string) string {
+	res, err := s.rdb.Get(context.Background(), s.key(key)).Result()
+	if err == redis.Nil {
+		return ""
+	}
+
+	return res
+}
+
+// Set insert or replace the value for the given key.
+func (s *RedisStore) Set(key, value string, expiration time.Duration) error {
+	_, err := s.rdb.Set(context.Background(), s.key(key), value, expiration).Result()
+	return err
+}
+
+// Del removes the given key.
+func (s *RedisStore) Del(key string) error {
+	_, err := s.rdb.Del(context.Background(), s.key(key)).Result()
+	return err
+}
+
+// MemStore is a KvStore implementation using a simple in memory map.
+type MemStore struct {
+	sync.Mutex
+	data map[string]string
+}
+
+// NewMemStore returns a MemStore instance.
+func NewMemStore() *MemStore {
+	return &MemStore{
+		data: make(map[string]string),
+	}
+}
+
+// Get returns a value for the given key. Returns an empty string when the
+// value does not exist.
+func (s *MemStore) Get(key string) string {
+	s.Lock()
+	defer s.Unlock()
+	return s.data[key]
+}
+
+// Set insert or replace the value for the given key.
+func (s *MemStore) Set(key, value string, _ time.Duration) error {
+	s.Lock()
+	defer s.Unlock()
+	s.data[key] = value
+	return nil
+}
+
+// Del removes the given key.
+func (s *MemStore) Del(key string) error {
+	s.Lock()
+	defer s.Unlock()
+	delete(s.data, key)
+	return nil
+}
+
+// Clear deletes everything in the memory store.
+func (s *MemStore) Clear() {
+	s.Lock()
+	defer s.Unlock()
+	s.data = make(map[string]string)
+}
---
+++ pkg/superbus/tasks.go
@@ -0,0 +1,301 @@
+package superbus
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+type (
+	// Operation is the event sent when we launch a task
+	Operation struct {
+		Name string      `json:"name"`
+		ID   interface{} `json:"id"`
+	}
+
+	// Payload is the stored content of a task
+	Payload struct {
+		ID    uuid.UUID `json:"id"`
+		Delay int       `json:"delay"`
+		Data  []byte    `json:"data"`
+	}
+
+	// TaskHandler is the function called on a task
+	TaskHandler func(*Operation, *Payload)
+
+	// TaskManager is the task manager
+	TaskManager struct {
+		sync.Mutex
+
+		em          EventManager
+		store       Store
+		handlers    map[string]TaskHandler
+		queue       chan func()
+		numWorkers  int
+		workerGroup *sync.WaitGroup
+		timerGroup  *sync.WaitGroup
+		keyPrefix   string
+	}
+
+	// TaskManagerOption is a function that sets TaskManager option upon creation.
+	TaskManagerOption func(*TaskManager)
+
+	// TaskOption is a function that sets Task option upon creation.
+	TaskOption func(t *Task)
+
+	// Task is a task shortcut
+	Task struct {
+		tm             *TaskManager
+		name           string
+		delay          int
+		unmarshallData func(data []byte) interface{}
+		taskHandler    func(data interface{})
+	}
+)
+
+// NewTaskManager creates a new TaskManager instance.
+func NewTaskManager(m EventManager, s Store, options ...TaskManagerOption) *TaskManager {
+	tm := &TaskManager{
+		em:          m,
+		store:       s,
+		handlers:    make(map[string]TaskHandler),
+		queue:       make(chan func()),
+		numWorkers:  1,
+		workerGroup: &sync.WaitGroup{},
+		timerGroup:  &sync.WaitGroup{},
+		keyPrefix:   "tasks",
+	}
+
+	for _, o := range options {
+		o(tm)
+	}
+
+	tm.em.On("task", tm.onTask)
+
+	return tm
+}
+
+// WithNumWorkers set the number of worker processes that handle operations.
+func WithNumWorkers(m int) TaskManagerOption {
+	return func(tm *TaskManager) {
+		tm.numWorkers = m
+	}
+}
+
+// WithOperationPrefix sets a prefix for all operation payloads.
+func WithOperationPrefix(p string) TaskManagerOption {
+	return func(tm *TaskManager) {
+		tm.keyPrefix = p
+	}
+}
+
+// onTask is the task's event handler
+func (tm *TaskManager) onTask(e Event) {
+	var op Operation
+	if err := json.Unmarshal(e.Value, &op); err != nil {
+		log.WithError(err).Error()
+		return
+	}
+	l := log.WithField("operation", op).WithField("event", e.Name)
+	l.Debug("received event")
+
+	// Find the task's payload
+	p0, err := tm.getPayload(&op)
+	if err != nil {
+		l.WithError(err).Debug()
+		return
+	}
+
+	// If there's no handler, clean everything up
+	f, ok := tm.handlers[op.Name]
+	if !ok {
+		tm.delPayload(&op)
+		l.Error("no handler")
+		return
+	}
+
+	// Enqueue the task to the queue
+	tm.timerGroup.Add(1)
+	time.AfterFunc(time.Second*time.Duration(p0.Delay), func() {
+		defer tm.timerGroup.Done()
+
+		// Fetch the payload
+		// If the payload is gone, the task was canceled
+		p1, err := tm.getPayload(&op)
+		if err != nil {
+			l.WithError(err).Error()
+			return
+		}
+
+		// Check the original and current payload IDs. When they
+		// don't match, the timer is running for a previous task and
+		// we don't need it anymore.
+		if p0.ID != p1.ID {
+			l.Error("not matching payloads")
+			return
+		}
+
+		// The payload can be removed when we're done.
+		defer tm.delPayload(&op)
+
+		// Push the worker to the queue.
+		tm.queue <- func() {
+			defer func() {
+				if r := recover(); r != nil {
+					l.Errorf("task error: %v", r)
+				}
+			}()
+			f(&op, &p1)
+		}
+	})
+}
+
+// getOperationKey returns the store key for an operation.
+func (tm *TaskManager) getOperationKey(name string, id interface{}) string {
+	return fmt.Sprintf("%s:%s:%v", tm.keyPrefix, name, id)
+}
+
+// getPayload returns the operation's payload after retrieving it from the store.
+func (tm *TaskManager) getPayload(op *Operation) (payload Payload, err error) {
+	data := tm.store.Get(tm.getOperationKey(op.Name, op.ID))
+	if data == "" {
+		err = errors.New("payload not found")
+		return
+	}
+	err = json.Unmarshal([]byte(data), &payload)
+	return
+}
+
+// delPayload removes the operation's payload from the store.
+func (tm *TaskManager) delPayload(t *Operation) {
+	tm.store.Del(tm.getOperationKey(t.Name, t.ID))
+}
+
+// Start starts the events listener and the process workers.
+func (tm *TaskManager) Start() {
+	go tm.em.Listen()
+	for i := 0; i < tm.numWorkers; i++ {
+		tm.workerGroup.Add(1)
+		go func(id int) {
+			defer tm.workerGroup.Done()
+			for task := range tm.queue {
+				task()
+			}
+		}(i)
+	}
+}
+
+// Stop stops the event listener and wait for running tasks to finish.
+func (tm *TaskManager) Stop() {
+	// Stop the event bus (can't receive any new event)
+	tm.em.Stop()
+
+	// Wait for all timers
+	tm.timerGroup.Wait()
+
+	// Stop the worker group
+	close(tm.queue)
+	tm.workerGroup.Wait()
+}
+
+// Launch sends a task order for later launch.
+func (tm *TaskManager) Launch(name string, id interface{}, delay int, data interface{}) error {
+	t := Operation{
+		Name: name,
+		ID:   id,
+	}
+
+	// Store the payload
+	payload := Payload{
+		ID:    uuid.New(),
+		Delay: delay,
+	}
+	var err error
+	if payload.Data, err = json.Marshal(data); err != nil {
+		return err
+	}
+
+	p, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	err = tm.store.Set(tm.getOperationKey(name, id), string(p), time.Duration(delay)+time.Second*30)
+	if err != nil {
+		return err
+	}
+
+	// Send the event
+	e, _ := json.Marshal(t)
+	return tm.em.Push("task", e)
+}
+
+// Register registers a task handler.
+func (tm *TaskManager) Register(name string, f TaskHandler) {
+	tm.Lock()
+	defer tm.Unlock()
+
+	tm.handlers[name] = f
+}
+
+// NewTask creates a new Task instance.
+func (tm *TaskManager) NewTask(name string, options ...TaskOption) Task {
+	t := Task{
+		tm:    tm,
+		name:  name,
+		delay: 0,
+	}
+
+	for _, o := range options {
+		o(&t)
+	}
+	tm.Register(t.name, func(_ *Operation, p *Payload) {
+		var data interface{} = p.Data
+		if t.unmarshallData != nil {
+			data = t.unmarshallData(p.Data)
+		}
+		t.taskHandler(data)
+	})
+
+	return t
+}
+
+// WithTaskHandler adds the given handler to the task.
+func WithTaskHandler(f func(data interface{})) TaskOption {
+	return func(t *Task) {
+		t.taskHandler = f
+	}
+}
+
+// WithUnmarshall registers a function that is responsible for payload decoding.
+func WithUnmarshall(f func(data []byte) interface{}) TaskOption {
+	return func(t *Task) {
+		t.unmarshallData = f
+	}
+}
+
+// WithTaskDelay sets the task's delay.
+func WithTaskDelay(d int) TaskOption {
+	return func(t *Task) {
+		t.delay = d
+	}
+}
+
+// Run launches the task.
+func (t Task) Run(id interface{}, data interface{}) error {
+	return t.tm.Launch(t.name, id, t.delay, data)
+}
+
+// Cancel removes the task's payload, effectively canceling it.
+func (t Task) Cancel(id interface{}) error {
+	return t.tm.store.Del(t.tm.getOperationKey(t.name, id))
+}
+
+// IsRunning returns true if the task is currently running or in the queue.
+func (t Task) IsRunning(id interface{}) bool {
+	return t.tm.store.Get(t.tm.getOperationKey(t.name, id)) != ""
+}

8298bcd274facc4a053ba7bd5a1bc933fc0f1bcc
Olivier Meunier <olivier@neokraft.net>
2021-09-30 00:03:59 +0200

    Avoid error if the bookmark's container can´t be open in debug mode.
    When a bookmark has just been created (and JS is disabled), the
    container does not exist and an error is triggered if the user has
    the DebugInfo flag.

--- internal/bookmarks/views.go
+++ internal/bookmarks/views.go
@@ -135,22 +135,26 @@ func (h *bookmarkViews) bookmarkInfo(w http.ResponseWriter, r *http.Request) {
 	}
 	ctx["Count"] = count
 
+	// Load bookmark debug information if the user needs them.
 	if user.Settings.DebugInfo {
 		c, err := b.openContainer()
-		if err != nil {
+		if err != nil && !os.IsNotExist(err) {
 			h.srv.Error(w, r, err)
 			return
 		}
-		defer c.Close()
 
-		for k, x := range map[string]string{
-			"_props": "props.json",
-			"_log":   "log",
-		} {
-			if r, err := c.GetFile(x); err != nil {
-				ctx[k] = err.Error()
-			} else {
-				ctx[k] = string(r)
+		if c != nil {
+			defer c.Close()
+
+			for k, x := range map[string]string{
+				"_props": "props.json",
+				"_log":   "log",
+			} {
+				if r, err := c.GetFile(x); err != nil {
+					ctx[k] = err.Error()
+				} else {
+					ctx[k] = string(r)
+				}
 			}
 		}
 	}

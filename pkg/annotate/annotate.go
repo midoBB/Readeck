d6fdf31c2372289a9922866c7d86733848c4fb8c
Olivier Meunier <olivier@neokraft.net>
2023-05-22 21:51:58 +0200

    Added pkg/annotate
    This package adds annotations to a DOM node based on a specific
    selector format.
    The containers are always Element nodes (not text nodes), with offsets
    based on the full text content of said nodes.
    This way, when we wrap annotation text nodes, the document's structure
    is changed but any annotation is still valid.

---
+++ pkg/annotate/annotate.go
@@ -0,0 +1,321 @@
+package annotate
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/antchfx/htmlquery"
+	"github.com/go-shiori/dom"
+	"golang.org/x/net/html"
+)
+
+const defaultTagName = "x-annotation"
+
+type nodeList []*html.Node
+
+// Annotation holds raw information about an annotation. It contains only selectors and offset used
+// to find the relevant text nodes when looking up for a range.
+type Annotation struct {
+	root          *html.Node
+	tagName       string
+	startSelector string
+	startOffset   int
+	endSelector   string
+	endOffset     int
+}
+
+// AnnotationRange holds the necessary DOM information to find and wrap an full annotation on
+// a node.
+type AnnotationRange struct {
+	root           *html.Node
+	textNodes      nodeList
+	ancestor       *html.Node
+	startContainer *html.Node
+	startOffset    int
+	endContainer   *html.Node
+	endOffset      int
+}
+
+// WrapCallback is a function called on each annotation wrapping node.
+// As an annotation can be covered by several wrapping nodes, an index gives the
+// current wrapping position.
+type WrapCallback func(node *html.Node, index int)
+
+// AddAnnotation is the main function that will add an annotation to a given root node.
+// It lets one set an annotation tag name and performs the overlap validation.
+// The root node is modified in place.
+func AddAnnotation(
+	root *html.Node, tagName string,
+	startSelector string, startOffset int,
+	endSelector string, endOffset int,
+	options ...WrapCallback,
+) error {
+	a := NewAnnotation(root, startSelector, startOffset, endSelector, endOffset)
+	r, err := a.ToRange(func(ar *AnnotationRange) error {
+		for _, n := range ar.textNodes {
+			if n.Parent.Data == tagName {
+				return fmt.Errorf("overlapping annotation")
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	r.Wrap(append(options, func(n *html.Node, index int) {
+		// Allways set the tag name, regardless of previously applied modifiers
+		n.Data = tagName
+	})...)
+	return nil
+}
+
+// NewAnnotation creates a new Annotation instance.
+func NewAnnotation(root *html.Node, startSelector string, startOffset int, endSelector string, endOffset int) *Annotation {
+	return &Annotation{
+		root:          root,
+		tagName:       defaultTagName,
+		startSelector: startSelector,
+		startOffset:   startOffset,
+		endSelector:   endSelector,
+		endOffset:     endOffset,
+	}
+}
+
+// ToRange returns AnnotationRange instance from the current Annotation.
+// This method finds the text nodes, common ancestor and validates that the
+// range can be wraped later.
+func (a *Annotation) ToRange(validators ...func(*AnnotationRange) error) (r *AnnotationRange, err error) {
+	if a.root == nil {
+		return nil, fmt.Errorf("root node is not defined")
+	}
+
+	r = &AnnotationRange{root: a.root}
+
+	// Get range boundaries
+	if r.startContainer, r.startOffset, err = getTextNodeBoundary(a.root, a.startSelector, a.startOffset); err != nil {
+		return nil, err
+	}
+
+	if r.endContainer, r.endOffset, err = getTextNodeBoundary(a.root, a.endSelector, a.endOffset); err != nil {
+		return nil, err
+	}
+
+	// Find common ancestor node
+	startParents := getParents(r.startContainer)
+	endParents := getParents(r.endContainer)
+	for i, node := range startParents {
+		if i >= len(endParents) || endParents[i] != node {
+			break
+		}
+		r.ancestor = node
+	}
+
+	// collect all text nodes included in range
+	started := false
+	done := false
+	walkTextNodes(r.ancestor, func(n *html.Node) {
+		if done {
+			return
+		}
+		if n == r.startContainer {
+			started = true
+		}
+		if started {
+			r.textNodes = append(r.textNodes, n)
+		}
+		if n == r.endContainer {
+			done = true
+		}
+	})
+
+	// an empty text node list can be because the start element is after the end element
+	// (it won't work)
+	if len(r.textNodes) == 0 {
+		return nil, fmt.Errorf("no text nodes in range")
+	}
+
+	// when only one node, check boundaries are ok
+	if len(r.textNodes) == 1 && r.startOffset > r.endOffset {
+		return nil, fmt.Errorf("invalid range")
+	}
+
+	for _, v := range validators {
+		if err = v(r); err != nil {
+			return nil, err
+		}
+	}
+
+	return
+}
+
+// Wrap insert annotation elements around the range text nodes
+func (r *AnnotationRange) Wrap(options ...WrapCallback) {
+	for i, node := range r.textNodes {
+		// Start offset
+		s := 0
+		if i == 0 {
+			s = r.startOffset
+		}
+
+		// End offset
+		e := len([]rune(node.Data))
+		if i+1 == len(r.textNodes) {
+			e = r.endOffset
+		}
+
+		// Both start and end offset can be on the same node, it
+		// does not matter.
+		wrapTextNode(node, s, e, func(n *html.Node) {
+			for _, f := range options {
+				f(n, i)
+			}
+		})
+	}
+}
+
+// getTextNodeBoundary returns a range (text node and offset), given a specific selector and an offset.
+// The offset parameter is from the very beginning of the selector.
+func getTextNodeBoundary(root *html.Node, selector string, index int) (*html.Node, int, error) {
+	e, err := htmlquery.Query(root, fmt.Sprintf("./%s", selector))
+	if err != nil {
+		return nil, 0, err
+	}
+
+	if e == nil {
+		return nil, 0, fmt.Errorf(`element "%s" not found`, selector)
+	}
+	var textNode *html.Node
+	offset := 0
+	consummed := 0
+	walkTextNodes(e, func(n *html.Node) {
+		if textNode != nil {
+			return
+		}
+		runesLength := len([]rune(n.Data))
+		if index <= consummed+runesLength {
+			offset = index - consummed
+			textNode = n
+		}
+		consummed += runesLength
+	})
+
+	if textNode == nil {
+		return nil, 0, fmt.Errorf(`index "%d" is out of range`, index)
+	}
+
+	return textNode, offset, nil
+}
+
+// getSelector returns a CSS selector for a text node at the given offset.
+func getSelector(root *html.Node, node *html.Node, offset int) (string, int, error) {
+	if node.Type != html.TextNode {
+		return "", 0, fmt.Errorf("node is not a text node")
+	}
+
+	p := node.Parent
+	names := []string{}
+
+	// Get selector
+	for p.Parent != nil && p != root {
+		i := 1
+		s := p
+		for dom.PreviousElementSibling(s) != nil {
+			s = dom.PreviousElementSibling(s)
+			if dom.TagName(s) == dom.TagName(p) {
+				i++
+			}
+		}
+		names = append(names, "")
+		copy(names[1:], names)
+		names[0] = fmt.Sprintf("%s[%d]", dom.TagName(p), i)
+		p = p.Parent
+	}
+
+	// Get offset
+	done := false
+	newOffset := 0
+	walkTextNodes(node.Parent, func(n *html.Node) {
+		if done {
+			return
+		}
+		if n == node {
+			done = true
+		}
+		if !done {
+			newOffset += len([]rune(n.Data))
+		} else {
+			newOffset += offset
+		}
+	})
+
+	return strings.Join(names, "/"), newOffset, nil
+}
+
+// getParents returns the parents of a node, from top to bottom
+func getParents(node *html.Node) nodeList {
+	p := node.Parent
+	res := nodeList{}
+	for p != nil {
+		res = append(res, nil)
+		copy(res[1:], res)
+		res[0] = p
+		p = p.Parent
+	}
+	return res
+}
+
+// walkTextNodes calls a callback function on each text node
+// found in a given node and its descendants.
+func walkTextNodes(node *html.Node, callback func(*html.Node)) {
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		if child.Type == html.TextNode {
+			callback(child)
+		}
+		walkTextNodes(child, callback)
+	}
+}
+
+// wrapTextNode replace the given text node with a wrap text node
+// and surrounding text nodes when the offsets have content.
+// It can take any func(*html.Node) to modify the resulting wrapping node
+// (change the tag name, add attributes...)
+func wrapTextNode(node *html.Node, startOffset, endOffset int, options ...func(*html.Node)) {
+	if node == nil || node.Parent == nil || node.Type != html.TextNode {
+		return
+	}
+	runes := []rune(node.Data)
+
+	if startOffset > endOffset ||
+		startOffset < 0 ||
+		endOffset < 0 ||
+		startOffset > len(runes) ||
+		endOffset > len(runes) {
+		return
+	}
+
+	var ts *html.Node
+	var te *html.Node
+	res := dom.CreateElement(defaultTagName)
+
+	if startOffset > 0 && startOffset <= len(runes) {
+		ts = dom.CreateTextNode(string(runes[0:startOffset]))
+	}
+	if endOffset >= 0 && endOffset <= len(runes) {
+		te = dom.CreateTextNode(string(runes[endOffset:]))
+	}
+
+	tx := dom.CreateTextNode(string(runes[startOffset:endOffset]))
+	dom.AppendChild(res, tx)
+	dom.ReplaceChild(node.Parent, res, node)
+
+	if ts != nil {
+		res.Parent.InsertBefore(ts, res)
+	}
+	if te != nil {
+		res.Parent.InsertBefore(te, res.NextSibling)
+	}
+
+	for _, option := range options {
+		option(res)
+	}
+}
---
+++ pkg/annotate/annotate_test.go
@@ -0,0 +1,645 @@
+package annotate
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	"golang.org/x/net/html"
+
+	"github.com/antchfx/htmlquery"
+	"github.com/go-shiori/dom"
+	"github.com/stretchr/testify/assert"
+)
+
+func loadDocument() *html.Node {
+	fd, err := os.Open("fixtures/test.html")
+	if err != nil {
+		panic(err)
+	}
+
+	doc, err := html.Parse(fd)
+	fd.Close()
+	if err != nil {
+		panic(err)
+	}
+	return doc
+}
+
+func TestFunctions(t *testing.T) {
+	t.Run("getTextNodeBoundary", func(t *testing.T) {
+		doc := loadDocument()
+		root := dom.QuerySelector(doc, "body")
+		tests := []struct {
+			selector  string
+			offset    int
+			node      *html.Node
+			newOffset int
+			err       string
+		}{
+			{
+				"../@", 0, nil, 0,
+				"expression must evaluate to a node-set",
+			},
+			{
+				"div[2]", 0, nil, 0,
+				`element "div[2]" not found`,
+			},
+			{
+				"p[1]", 260, nil, 0,
+				`index "260" is out of range`,
+			},
+			{
+				"p[1]", 0,
+				htmlquery.FindOne(doc, "/html/body/p[1]/text()[1]"), 0,
+				"",
+			},
+			{
+				"p[1]", 5,
+				htmlquery.FindOne(doc, "/html/body/p[1]/text()[1]"), 5,
+				"",
+			},
+			{
+				"p[2]", 30,
+				htmlquery.FindOne(doc, "/html/body/p[2]/text()[2]"), 12,
+				"",
+			},
+			{
+				"div[1]/p[2]/strong[1]", 27,
+				htmlquery.FindOne(doc, "/html/body/div[1]/p[2]/strong[1]/text()[2]"), 1,
+				"",
+			},
+			{
+				"p[3]", 179,
+				htmlquery.FindOne(doc, "/html/body/p[3]/text()[2]"), 6,
+				"",
+			},
+		}
+
+		for i, test := range tests {
+			t.Run(strconv.Itoa(i), func(t *testing.T) {
+				node, offset, err := getTextNodeBoundary(root, test.selector, test.offset)
+				if test.err != "" {
+					assert.EqualError(t, err, test.err)
+				} else {
+					assert.Nil(t, err)
+					assert.Equal(t, html.TextNode, node.Type)
+					assert.Equal(t, test.node, node)
+					assert.Equal(t, test.newOffset, offset)
+				}
+			})
+
+		}
+	})
+
+	t.Run("getSelector", func(t *testing.T) {
+		doc := loadDocument()
+		root := dom.QuerySelector(doc, "body")
+		tests := []struct {
+			node     *html.Node
+			child    int
+			selector string
+			offset   int
+			err      string
+		}{
+			{
+				htmlquery.FindOne(doc, "/html/body/p[1]"), 0,
+				"", 0,
+				"node is not a text node",
+			},
+			{
+				htmlquery.FindOne(doc, "/html/body/p[1]/text()[1]"), 0,
+				"p[1]", 0,
+				"",
+			},
+			{
+				htmlquery.FindOne(doc, "/html/body/p[1]/text()[1]"), 5,
+				"p[1]", 5,
+				"",
+			},
+			{
+				htmlquery.FindOne(doc, "/html/body/p[2]/text()[1]"), 7,
+				"p[2]", 7,
+				"",
+			},
+			{
+				htmlquery.FindOne(doc, "/html/body/p[2]/text()[2]"), 12,
+				"p[2]", 30,
+				"",
+			},
+			{
+				htmlquery.FindOne(doc, "/html/body/div[1]/p[2]/strong[1]/text()[2]"), 1,
+				"div[1]/p[2]/strong[1]", 27,
+				"",
+			},
+			{
+				htmlquery.FindOne(doc, "/html/body/p[3]/text()[1]"), 100,
+				"p[3]", 100,
+				"",
+			},
+			{
+				htmlquery.FindOne(doc, "/html/body/p[3]/text()[2]"), 9,
+				"p[3]", 182,
+				"",
+			},
+		}
+
+		for i, test := range tests {
+			t.Run(strconv.Itoa(i), func(t *testing.T) {
+				selector, offset, err := getSelector(root, test.node, test.child)
+				if test.err != "" {
+					assert.EqualError(t, err, test.err)
+				} else {
+					assert.Nil(t, err)
+					assert.Equal(t, test.selector, selector)
+					assert.Equal(t, test.offset, offset)
+				}
+			})
+		}
+	})
+
+	t.Run("wrapTextNode", func(t *testing.T) {
+		contents := `<body>
+		<p>Loren ipsum <strong>dolor</strong> sit amet</p>
+		<p>“Med hjälp av en text som denna så ser man snabbt hur text kan placeras och ‘hur det därefter ser ut’”</p>
+		</body>`
+
+		tests := []struct {
+			selector       string
+			start          int
+			end            int
+			options        []func(*html.Node)
+			resultSelector string
+			result         string
+		}{
+			{
+				"./p[1]/text()[1]", 0, 0, nil,
+				"./p[1]",
+				"<p><x-annotation></x-annotation>Loren ipsum <strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[1]/text()[1]", 0, 12, nil,
+				"./p[1]",
+				"<p><x-annotation>Loren ipsum </x-annotation><strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[1]/text()[1]", 6, 12, nil,
+				"./p[1]",
+				"<p>Loren <x-annotation>ipsum </x-annotation><strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[1]/text()[1]", 0, 13, nil,
+				"./p[1]",
+				"<p>Loren ipsum <strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[1]/text()[2]", 9, 10, nil,
+				"./p[1]",
+				"<p>Loren ipsum <strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[1]/text()[1]", 1, 0, nil,
+				"./p[1]",
+				"<p>Loren ipsum <strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[1]/text()[1]", -1, 13, nil,
+				"./p[1]",
+				"<p>Loren ipsum <strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[1]/text()[1]", 0, -1, nil,
+				"./p[1]",
+				"<p>Loren ipsum <strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[2]/text()[1]", 1, 4, nil,
+				"./p[2]",
+				"<p>“<x-annotation>Med</x-annotation> hjälp av en text som denna så ser man snabbt hur text kan placeras och ‘hur det därefter ser ut’”</p>",
+			},
+			{
+				"./p[2]/text()[1]", 72, 102, nil,
+				"./p[2]",
+				"<p>“Med hjälp av en text som denna så ser man snabbt hur text kan placeras <x-annotation>och ‘hur det därefter ser ut’”</x-annotation></p>",
+			},
+			{
+				"./p[1]", 0, 4, nil,
+				"./p[1]",
+				"<p>Loren ipsum <strong>dolor</strong> sit amet</p>",
+			},
+			{
+				"./p[1]/text()[1]", 6, 12, []func(*html.Node){
+					func(n *html.Node) {
+						n.Data = "span"
+					},
+					func(n *html.Node) {
+						dom.SetAttribute(n, "class", "annotation")
+					},
+				},
+				"./p[1]",
+				`<p>Loren <span class="annotation">ipsum </span><strong>dolor</strong> sit amet</p>`,
+			},
+		}
+
+		for _, test := range tests {
+			t.Run(fmt.Sprintf("%d:%d", test.start, test.end), func(t *testing.T) {
+				root, err := html.Parse(strings.NewReader(contents))
+				if err != nil {
+					panic(err)
+				}
+				n := htmlquery.FindOne(dom.QuerySelector(root, "body"), test.selector)
+				wrapTextNode(n, test.start, test.end, test.options...)
+
+				s := bytes.Buffer{}
+				html.Render(&s, htmlquery.FindOne(dom.QuerySelector(root, "body"), test.resultSelector))
+				assert.Equal(t, test.result, s.String())
+			})
+		}
+	})
+}
+
+func TestAnnotation(t *testing.T) {
+	t.Run("annotation", func(t *testing.T) {
+		doc := loadDocument()
+		root := dom.QuerySelector(doc, "body")
+
+		tests := []struct {
+			startSelector string
+			startOffset   int
+			endSelector   string
+			endOffset     int
+			expected      *AnnotationRange
+			err           string
+		}{
+			{
+				"p[1]", 260,
+				"p[1]", 270,
+				nil,
+				`index "260" is out of range`,
+			},
+			{
+				"p[1]", 0,
+				"p[1]", 270,
+				nil,
+				`index "270" is out of range`,
+			},
+			{
+				"p[2]", 19,
+				"p[1]", 24,
+				nil,
+				"no text nodes in range",
+			},
+			{
+				"p[1]", 24,
+				"p[1]", 19,
+				nil,
+				"invalid range",
+			},
+			{
+				"p[1]", 19,
+				"p[1]", 24,
+				&AnnotationRange{
+					root,
+					nodeList{htmlquery.FindOne(root, "./p[1]/text()[1]")},
+					htmlquery.FindOne(root, "./p[1]"),
+					htmlquery.FindOne(root, "./p[1]/text()[1]"), 19,
+					htmlquery.FindOne(root, "./p[1]/text()[1]"), 24,
+				},
+				"",
+			},
+			{
+				"h2[1]/span[1]", 0,
+				"p[2]/b[1]", 5,
+				&AnnotationRange{
+					root,
+					nodeList{
+						htmlquery.FindOne(root, "./h2[1]/span[1]/text()[1]"),
+						htmlquery.FindOne(root, "./text()[3]"),
+						htmlquery.FindOne(root, "./p[2]/text()[1]"),
+						htmlquery.FindOne(root, "./p[2]/b[1]/text()[1]"),
+					},
+					root,
+					htmlquery.FindOne(root, "./h2[1]/span[1]/text()[1]"), 0,
+					htmlquery.FindOne(root, "./p[2]/b[1]/text()[1]"), 5,
+				},
+				"",
+			},
+			{
+				"div[1]/p[2]", 66,
+				"div[1]/p[2]", 122,
+				&AnnotationRange{
+					root,
+					nodeList{
+						htmlquery.FindOne(root, "./div[1]/p[2]/text()[1]"),
+						htmlquery.FindOne(root, "./div[1]/p[2]/strong[1]/text()[1]"),
+						htmlquery.FindOne(root, "./div[1]/p[2]/strong[1]/a[1]/text()[1]"),
+						htmlquery.FindOne(root, "./div[1]/p[2]/strong[1]/text()[2]"),
+						htmlquery.FindOne(root, "./div[1]/p[2]/text()[2]"),
+					},
+					htmlquery.FindOne(root, "./div[1]/p[2]"),
+					htmlquery.FindOne(root, "./div[1]/p[2]/text()[1]"), 66,
+					htmlquery.FindOne(root, "./div[1]/p[2]/text()[2]"), 13,
+				},
+				"",
+			},
+		}
+
+		for i, test := range tests {
+			t.Run(strconv.Itoa(i), func(t *testing.T) {
+				a := NewAnnotation(root, test.startSelector, test.startOffset, test.endSelector, test.endOffset)
+				r, err := a.ToRange()
+				if test.err != "" {
+					assert.EqualError(t, err, test.err)
+				} else {
+					assert.Nil(t, err)
+					assert.Equal(t, test.expected, r)
+				}
+			})
+		}
+	})
+
+	t.Run("range validator", func(t *testing.T) {
+		doc := loadDocument()
+		root := dom.QuerySelector(doc, "body")
+
+		tests := []struct {
+			startSelector string
+			startOffset   int
+			endSelector   string
+			endOffset     int
+			validators    []func(r *AnnotationRange) error
+			err           string
+		}{
+			{
+				"p[1]", 19,
+				"p[1]", 24,
+				[]func(r *AnnotationRange) error{
+					func(r *AnnotationRange) error {
+						return nil
+					},
+				},
+				"",
+			},
+			{
+				"p[1]", 19,
+				"p[1]", 24,
+				[]func(r *AnnotationRange) error{
+					func(r *AnnotationRange) error {
+						return nil
+					},
+					func(r *AnnotationRange) error {
+						return errors.New("invalid range")
+					},
+				},
+				"invalid range",
+			},
+			{
+				"p[1]", 19,
+				"p[1]", 24,
+				[]func(r *AnnotationRange) error{
+					func(r *AnnotationRange) error {
+						return nil
+					},
+					func(r *AnnotationRange) error {
+						return errors.New("error 1")
+					},
+					func(r *AnnotationRange) error {
+						return errors.New("error 2")
+					},
+				},
+				"error 1",
+			},
+		}
+
+		for i, test := range tests {
+			t.Run(strconv.Itoa(i), func(t *testing.T) {
+				a := NewAnnotation(root, test.startSelector, test.startOffset, test.endSelector, test.endOffset)
+				r, err := a.ToRange(test.validators...)
+				if test.err != "" {
+					assert.Nil(t, r)
+					assert.EqualError(t, err, test.err)
+				} else {
+					assert.Nil(t, err)
+				}
+			})
+		}
+
+	})
+
+	t.Run("range on null root", func(t *testing.T) {
+		a := NewAnnotation(nil, "", 0, "", 0)
+		r, err := a.ToRange()
+		assert.Nil(t, r)
+		assert.EqualError(t, err, "root node is not defined")
+	})
+
+	t.Run("wrap", func(t *testing.T) {
+		tests := []struct {
+			annotations []*Annotation
+			expected    [][2]string
+		}{
+			{
+				[]*Annotation{
+					NewAnnotation(nil, "p[1]", 19, "p[1]", 24),
+				},
+				[][2]string{
+					{"./p[1]/x-annotation[1]", "dolor"},
+				},
+			},
+			{
+				[]*Annotation{
+					NewAnnotation(nil, "h2[1]/span[1]", 0, "p[2]/b[1]", 5),
+				},
+				[][2]string{
+					{"./h2[1]/span[1]/x-annotation[1]", "test"},
+					{"./x-annotation[1]", ""},
+					{"./p[2]/x-annotation[1]", "Lorem"},
+					{"./p[2]/b[1]/x-annotation[1]", "ipsum"},
+				},
+			},
+			{
+				[]*Annotation{
+					NewAnnotation(nil, "p[1]", 19, "p[1]", 24),
+					NewAnnotation(nil, "h2[1]/span[1]", 0, "p[2]/b[1]", 5),
+				},
+				[][2]string{
+					{"./p[1]/x-annotation[1]", "dolor"},
+					{"./h2[1]/span[1]/x-annotation[1]", "test"},
+					{"./x-annotation[1]", ""},
+					{"./p[2]/x-annotation[1]", "Lorem"},
+					{"./p[2]/b[1]/x-annotation[1]", "ipsum"},
+				},
+			},
+			{
+				[]*Annotation{
+					NewAnnotation(nil, "h2[1]/span[1]", 0, "p[2]/b[1]", 5),
+					NewAnnotation(nil, "p[1]", 19, "p[1]", 24),
+				},
+				[][2]string{
+					{"./p[1]/x-annotation[1]", "dolor"},
+					{"./h2[1]/span[1]/x-annotation[1]", "test"},
+					{"./x-annotation[1]", ""},
+					{"./p[2]/x-annotation[1]", "Lorem"},
+					{"./p[2]/b[1]/x-annotation[1]", "ipsum"},
+				},
+			},
+			{
+				[]*Annotation{
+					NewAnnotation(nil, "h2[1]/span[1]", 0, "p[2]/b[1]", 5),
+					NewAnnotation(nil, "p[1]", 19, "p[1]", 24),
+					NewAnnotation(nil, "div[1]/p[2]/strong[1]", 12, "div[1]/p[2]/strong[1]/a[1]", 3),
+				},
+				[][2]string{
+					{"./p[1]/x-annotation[1]", "dolor"},
+					{"./h2[1]/span[1]/x-annotation[1]", "test"},
+					{"./x-annotation[1]", ""},
+					{"./p[2]/x-annotation[1]", "Lorem"},
+					{"./p[2]/b[1]/x-annotation[1]", "ipsum"},
+					{"./div[1]/p[2]/strong[1]/x-annotation[1]", "sciunt"},
+					{"./div[1]/p[2]/strong[1]/a[1]/x-annotation[1]", "sit"},
+				},
+			},
+			{
+				[]*Annotation{
+					NewAnnotation(nil, "./p[3]", 174, "./p[3]", 204),
+				},
+				[][2]string{
+					{"./p[3]/x-annotation[1]", "och ‘hur det därefter ser ut’”"},
+				},
+			},
+			{
+				[]*Annotation{
+					NewAnnotation(nil, "./p[3]", 155, "./p[3]", 182),
+				},
+				[][2]string{
+					{"./p[3]/x-annotation[1]", "kan"},
+					{"./p[3]/b[1]/x-annotation[1]", "placeras"},
+					{"./p[3]/x-annotation[2]", "och ‘hur"},
+				},
+			},
+		}
+
+		for i, test := range tests {
+			t.Run(strconv.Itoa(i), func(t *testing.T) {
+				doc := loadDocument()
+				root := dom.QuerySelector(doc, "body")
+
+				for _, annotation := range test.annotations {
+					annotation.root = root
+					r, err := annotation.ToRange()
+					if err != nil {
+						panic(err)
+					}
+					r.Wrap()
+				}
+
+				// s := bytes.Buffer{}
+				// html.Render(&s, root)
+				// println(s.String())
+
+				nodes := htmlquery.Find(root, "//x-annotation")
+				assert.Equal(t, len(test.expected), len(nodes))
+
+				for _, expected := range test.expected {
+					n := htmlquery.FindOne(root, expected[0])
+					assert.NotNil(t, n)
+					assert.Equal(t, html.TextNode, n.FirstChild.Type)
+					assert.Equal(t, expected[1], strings.TrimSpace(n.FirstChild.Data))
+				}
+
+			})
+		}
+	})
+}
+
+func TestAddAnnotation(t *testing.T) {
+	tests := []struct {
+		annotations []*Annotation
+		expected    [][3]string
+		err         string
+	}{
+		{
+			[]*Annotation{
+				NewAnnotation(nil, "p[1]", 19, "p[1]", 24),
+			},
+			[][3]string{
+				{"./p[1]/my-annotation[1]", "dolor", "0"},
+			},
+			"",
+		},
+		{
+			[]*Annotation{
+				NewAnnotation(nil, "h2[1]/span[1]", 0, "p[2]/b[1]", 5),
+				NewAnnotation(nil, "p[1]", 19, "p[1]", 24),
+				NewAnnotation(nil, "div[1]/p[2]/strong[1]", 12, "div[1]/p[2]/strong[1]/a[1]", 3),
+			},
+			[][3]string{
+				{"./p[1]/my-annotation[1]", "dolor", "1"},
+				{"./h2[1]/span[1]/my-annotation[1]", "test", "0"},
+				{"./my-annotation[1]", "", "0"},
+				{"./p[2]/my-annotation[1]", "Lorem", "0"},
+				{"./p[2]/b[1]/my-annotation[1]", "ipsum", "0"},
+				{"./div[1]/p[2]/strong[1]/my-annotation[1]", "sciunt", "2"},
+				{"./div[1]/p[2]/strong[1]/a[1]/my-annotation[1]", "sit", "2"},
+			},
+			"",
+		},
+		{
+			[]*Annotation{
+				NewAnnotation(nil, "p[1]", 34, "p[1]", 62),
+				NewAnnotation(nil, "p[1]", 87, "p[1]", 109),
+			},
+			[][3]string{
+				{"./p[1]/my-annotation[1]", "consectetur adipisicing elit", "0"},
+				{"./p[1]/my-annotation[2]", "inventore a voluptatem", "1"},
+			},
+			"",
+		},
+		{
+			[]*Annotation{
+				NewAnnotation(nil, "p[1]", 34, "p[1]", 62),
+				NewAnnotation(nil, "p[1]", 87, "p[1]", 109),
+				NewAnnotation(nil, "p[1]", 58, "p[1]", 71),
+			},
+			[][3]string{},
+			"overlapping annotation",
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			doc := loadDocument()
+			root := dom.QuerySelector(doc, "body")
+
+			var err error
+			for j, a := range test.annotations {
+				err = AddAnnotation(root, "my-annotation", a.startSelector, a.startOffset, a.endSelector, a.endOffset, func(n *html.Node, index int) {
+					dom.SetAttribute(n, "data-annotation-id", strconv.Itoa(j))
+				})
+				if err != nil {
+					break
+				}
+			}
+
+			if test.err != "" {
+				assert.EqualError(t, err, test.err)
+				return
+			}
+
+			nodes := htmlquery.Find(root, "//my-annotation")
+			assert.Equal(t, len(test.expected), len(nodes))
+
+			for _, expected := range test.expected {
+				n := htmlquery.FindOne(root, expected[0])
+				assert.NotNil(t, n)
+				assert.Equal(t, html.TextNode, n.FirstChild.Type)
+				assert.Equal(t, expected[1], strings.TrimSpace(n.FirstChild.Data))
+				assert.Equal(t, expected[2], dom.GetAttribute(n, "data-annotation-id"))
+			}
+		})
+	}
+}

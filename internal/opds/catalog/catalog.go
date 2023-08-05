83f1b76a087611e1f8026dcf2ce22f6c269ee31e
Olivier Meunier <olivier@neokraft.net>
2023-08-05 16:51:47 +0200

    OPDS tooling
    - pkg/opds: collection of types to create a new OPDS catalog
    - internal/opds/catalog: helpers to work with the opds module

---
+++ internal/opds/catalog/catalog.go
@@ -0,0 +1,172 @@
+package catalog
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/readeck/readeck/internal/server"
+	"github.com/readeck/readeck/pkg/opds"
+)
+
+// Catalog is a wraper around opds.Feed
+type Catalog struct {
+	*opds.Feed
+}
+
+// New creates a new catalog with some prepared information.
+func New(srv *server.Server, r *http.Request, options ...func(*opds.Feed)) *Catalog {
+	feed := &opds.Feed{
+		FeedType: opds.OPDSTypeNavigation,
+		Links: []opds.Link{
+			{
+				Rel:      "start",
+				Href:     srv.AbsoluteURL(r, "/opds").String(),
+				TypeLink: opds.OPDSTypeNavigation,
+			},
+		},
+		Entries: []opds.Entry{},
+		Author: []opds.Author{
+			{
+				Name: "Readeck",
+				URI:  srv.AbsoluteURL(r, "/").String(),
+			},
+		},
+	}
+
+	for _, f := range options {
+		f(feed)
+	}
+
+	return &Catalog{feed}
+}
+
+// WithFeedType sets the feed type.
+func WithFeedType(t string) func(*opds.Feed) {
+	return func(feed *opds.Feed) {
+		feed.FeedType = t
+	}
+}
+
+// WithURL sets the "self" feed link entry.
+func WithURL(href string) func(*opds.Feed) {
+	return func(feed *opds.Feed) {
+		feed.ID = opds.URLID(href)
+		feed.Links = append(feed.Links, opds.Link{
+			Rel:      "self",
+			Href:     href,
+			TypeLink: opds.OPDSTypeNavigation,
+		})
+	}
+}
+
+// WithLink adds a link entry to the feed.
+func WithLink(t, rel, href string) func(*opds.Feed) {
+	return func(feed *opds.Feed) {
+		feed.Links = append(feed.Links, opds.Link{Rel: rel, Href: href, TypeLink: t})
+	}
+}
+
+// WithTitle sets the feed's title.
+func WithTitle(title string) func(*opds.Feed) {
+	return func(feed *opds.Feed) {
+		feed.Title = title
+	}
+}
+
+// WithUpdated sets the feed last update value.
+func WithUpdated(t time.Time) func(*opds.Feed) {
+	return func(feed *opds.Feed) {
+		feed.Updated = *opds.AtomDate(t)
+	}
+}
+
+// WithNavEntry adds a new navigation entry to the feed.
+func WithNavEntry(title string, updated time.Time, href string, options ...func(*opds.Entry)) func(*opds.Feed) {
+	return func(feed *opds.Feed) {
+		e := opds.Entry{
+			Title:      opds.SanitizeString(title),
+			Updated:    *opds.AtomDate(updated),
+			Content:    &opds.Content{ContentType: "text", Content: opds.SanitizeString(title)},
+			ID:         opds.URLID(href),
+			Identifier: opds.URLID(href),
+			Links: []opds.Link{
+				{
+					Rel:      "subsection",
+					Href:     href,
+					TypeLink: opds.OPDSTypeAcquisistion,
+				},
+			},
+		}
+		for _, f := range options {
+			f(&e)
+		}
+		feed.Entries = append(feed.Entries, e)
+	}
+}
+
+// WithBookEntry adds a new book entry to the feed.
+func WithBookEntry(
+	id uuid.UUID, title string, href string,
+	issued, published, updated time.Time,
+	publisher string, language string, description string,
+) func(*opds.Feed) {
+	return func(feed *opds.Feed) {
+		e := opds.Entry{
+			ID:         opds.ID(id),
+			Identifier: opds.ID(id),
+			Issued:     opds.AtomDate(issued),
+			Published:  opds.AtomDate(published),
+			Updated:    *opds.AtomDate(updated),
+			Title:      opds.SanitizeString(title),
+			Publisher:  opds.SanitizeString(publisher),
+			Language:   language,
+			Links: []opds.Link{
+				{
+					Rel:      "http://opds-spec.org/acquisition",
+					TypeLink: "application/epub+zip",
+					Href:     href,
+				},
+			},
+		}
+
+		if description != "" {
+			e.Content = &opds.Content{
+				ContentType: "html",
+				Content:     opds.SanitizeString(description),
+			}
+		}
+
+		feed.Entries = append(feed.Entries, e)
+	}
+
+	// Note: if we want to add an image
+	// e.Links = append(e.Links, opds.Link{
+	// 	Rel:  "http://opds-spec.org/image",
+	// 	Href: imgSrc,
+	// })
+
+}
+
+// Render write the full catalog to a writer.
+func (c *Catalog) Render(w http.ResponseWriter, r *http.Request) error {
+	buf := new(bytes.Buffer)
+	err := c.Encode(buf)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", c.Feed.FeedType)
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", buf.Len()))
+
+	if r.Method == http.MethodHead {
+		return nil
+	}
+
+	_, err = io.Copy(w, buf)
+	return err
+}
---
+++ pkg/opds/opds.go
@@ -0,0 +1,177 @@
+package opds
+
+import (
+	"encoding/xml"
+	"io"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+const (
+	// OPDSTypeNavigation is the link type for navigation
+	OPDSTypeNavigation = "application/atom+xml; profile=opds-catalog; kind=navigation"
+	// OPDSTypeAcquisistion is the link type for acquisition
+	OPDSTypeAcquisistion = "application/atom+xml; profile=opds-catalog; kind=acquisition"
+)
+
+// Feed root element for acquisition or navigation feed
+type Feed struct {
+	XMLName      xml.Name `xml:"feed"`
+	XMLns        string   `xml:"xmlns,attr"`
+	XMLnsDC      string   `xml:"xmlns:dc,attr"`
+	Lang         string   `xml:"xml:lang,attr"`
+	ID           UUID     `xml:"id"`
+	Title        string   `xml:"title"`
+	Author       []Author `xml:"author,omitempty"`
+	Updated      Time     `xml:"updated"`
+	Entries      []Entry  `xml:"entry"`
+	Links        []Link   `xml:"link"`
+	TotalResults int      `xml:"totalResults,omitempty"`
+	ItemsPerPage int      `xml:"itemsPerPage,omitempty"`
+	FeedType     string   `xml:"-"`
+}
+
+// Link link to different resources
+type Link struct {
+	Rel                 string                `xml:"rel,attr"`
+	Href                string                `xml:"href,attr"`
+	TypeLink            string                `xml:"type,attr,omitempty"`
+	Title               string                `xml:"title,attr,omitempty"`
+	FacetGroup          string                `xml:"facetGroup,attr,omitempty"`
+	Count               int                   `xml:"count,attr,omitempty"`
+	IndirectAcquisition []IndirectAcquisition `xml:"indirectAcquisition,omitempty"`
+}
+
+// Author represent the feed author or the entry author
+type Author struct {
+	Name string `xml:"name"`
+	URI  string `xml:"uri"`
+}
+
+// Entry an atom entry in the feed
+type Entry struct {
+	Title      string     `xml:"title"`
+	ID         UUID       `xml:"id"`
+	Identifier UUID       `xml:"dc:identifier"`
+	Updated    Time       `xml:"updated"`
+	Rights     string     `xml:"rights,omitempty"`
+	Publisher  string     `xml:"dc:publisher,omitempty"`
+	Author     []Author   `xml:"author,omitempty"`
+	Language   string     `xml:"dc:language,omitempty"`
+	Issued     *Time      `xml:"dc:issued,omitempty"`
+	Published  *Time      `xml:"published,omitempty"`
+	Category   []Category `xml:"category,omitempty"`
+	Links      []Link     `xml:"link,omitempty"`
+	Summary    *Content   `xml:"summary,omitempty"`
+	Content    *Content   `xml:"content,omitempty"`
+	Series     []Serie    `xml:"Series"`
+}
+
+// Content content tag in an entry, the type will be html or text
+type Content struct {
+	Content     string `xml:",cdata"`
+	ContentType string `xml:"type,attr"`
+}
+
+// Category represent the book category with scheme and term to machine
+// handling
+type Category struct {
+	Scheme string `xml:"scheme,attr"`
+	Term   string `xml:"term,attr"`
+	Label  string `xml:"label,attr"`
+}
+
+// IndirectAcquisition represent the link mostly for buying or borrowing
+// a book
+type IndirectAcquisition struct {
+	TypeAcquisition     string                `xml:"type,attr"`
+	IndirectAcquisition []IndirectAcquisition `xml:"indirectAcquisition"`
+}
+
+// Serie store serie information from schema.org
+type Serie struct {
+	Name     string  `xml:"name,attr"`
+	URL      string  `xml:"url,attr"`
+	Position float32 `xml:"position,attr"`
+}
+
+// NewLinkEntry creates a navigation link
+func NewLinkEntry(title string, updated time.Time, href string) Entry {
+	return Entry{
+		Title:      SanitizeString(title),
+		Updated:    *AtomDate(updated),
+		Content:    &Content{ContentType: "text", Content: SanitizeString(title)},
+		ID:         URLID(href),
+		Identifier: URLID(href),
+		Links: []Link{
+			{
+				Rel:      "subsection",
+				Href:     href,
+				TypeLink: OPDSTypeAcquisistion,
+			},
+		},
+	}
+}
+
+// SanitizeString replaces any control character in a string by a space
+func SanitizeString(s string) string {
+	res := []rune(s)
+	for i, x := range res {
+		if x <= 25 {
+			res[i] = rune(' ')
+		}
+	}
+
+	return string(res)
+}
+
+// UUID is a wrapper around uuid.UUID that can be marshaled into
+// a "urn:uuid" identifier.
+type UUID struct {
+	uuid.UUID
+}
+
+// ID returns a new UUID instance
+func ID(src uuid.UUID) UUID {
+	return UUID{src}
+}
+
+// URLID returns a new UUID instance, based on a URL.
+func URLID(src string) UUID {
+	return ID(uuid.NewMD5(uuid.NameSpaceURL, []byte(src)))
+}
+
+// MarshalText encoded an UUID into a "urn:uuid" format.
+func (id UUID) MarshalText() ([]byte, error) {
+	return []byte("urn:uuid:" + id.String()), nil
+}
+
+// Time is wrapper around time.Time that can marshal into an RFC3339 format.
+type Time struct {
+	time.Time
+}
+
+// AtomDate returns a new Time instance.
+func AtomDate(src time.Time) *Time {
+	return &Time{src}
+}
+
+// MarshalText encodes the Time instance in RFC3339 format.
+func (d Time) MarshalText() ([]byte, error) {
+	return []byte(d.Format(time.RFC3339)), nil
+}
+
+// Encode encodes a feed on a writer.
+func (f *Feed) Encode(w io.Writer) (err error) {
+	if _, err = io.WriteString(w, xml.Header); err != nil {
+		return
+	}
+
+	f.Lang = "en"
+	f.XMLns = "http://www.w3.org/2005/Atom"
+	f.XMLnsDC = "http://purl.org/dc/terms/"
+
+	enc := xml.NewEncoder(w)
+	return enc.Encode(f)
+}

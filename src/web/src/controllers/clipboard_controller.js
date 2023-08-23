79d13ade3979a3754e8cd821ca3cec72d55a5143
Olivier Meunier <olivier@neokraft.net>
2023-08-23 21:08:07 +0200

    Simplified clipboard controller.
    The button needs to be provided on the implementation side.

--- src/web/src/controllers/clipboard_controller.js
+++ src/web/src/controllers/clipboard_controller.js
@@ -3,22 +3,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import {Controller} from "@hotwired/stimulus"
-import $ from "../lib/dq"
-import icon from "../lib/icon"
 
 export default class extends Controller {
-  static targets = ["label", "content"]
-
-  connect() {
-    if (this.hasLabelTarget) {
-      $.E("button")
-        .addClass("text-primary", "hf:text-primary-dark")
-        .attr("type", "button")
-        .attr("data-action", `${this.identifier}#copy`)
-        .append(icon.getIcon("o-copy"))
-        .appendTo(this.labelTarget)
-    }
-  }
+  static targets = ["content"]
 
   async copy() {
     await navigator.clipboard.writeText(this.contentTarget.value)

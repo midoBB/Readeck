459da1774cf044d4196618aaa713668bc860bb1c
Olivier Meunier <olivier@neokraft.net>
2023-09-18 21:32:32 +0200

    Improved pkg/zipfs HTTPZipFile
    Instead of using the zip file descriptor to get the raw content, we
    can use OpenRaw on a zip.File.
    
    Added tests.

---
+++ pkg/zipfs/http.go
@@ -0,0 +1,154 @@
+// SPDX-FileCopyrightText: © 2020 Olivier Meunier <olivier@neokraft.net>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package zipfs
+
+import (
+	"archive/zip"
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/gabriel-vasile/mimetype"
+)
+
+// HTTPZipFile serves a zip file as an HTTP directory (without listing)
+// It properly handles If-Modified-Since header and can serve the compressed
+// content when deflate is in Accept-Encoding and the content is compressed
+// with deflate.
+type HTTPZipFile string
+
+type serveOption func(w http.ResponseWriter, status int)
+
+func (f HTTPZipFile) ServeHTTP(w http.ResponseWriter, r *http.Request, options ...serveOption) {
+	if r.Method != "GET" && r.Method != "HEAD" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	filename := r.URL.Path
+	zr, err := zip.OpenReader(string(f))
+	if err != nil {
+		f.error(w, err)
+		return
+	}
+	defer zr.Close()
+
+	zf, err := f.getEntry(filename, zr)
+	if err != nil {
+		f.error(w, err)
+		return
+	}
+
+	f.serveEntry(w, r, zf, options...)
+}
+
+// getEntry returns a zip file entry. It must be
+func (f HTTPZipFile) getEntry(name string, zr *zip.ReadCloser) (*zip.File, error) {
+	for _, x := range zr.File {
+		if x.Name == name && !x.FileInfo().IsDir() {
+			return x, nil
+		}
+	}
+	return nil, os.ErrNotExist
+}
+
+func (f HTTPZipFile) serveEntry(w http.ResponseWriter, r *http.Request, zf *zip.File, options ...serveOption) {
+	if f.checkIfModifiedSince(r, zf.Modified.UTC()) {
+		applyOptions(options, w, http.StatusNotModified)
+		w.WriteHeader(http.StatusNotModified)
+		return
+	}
+
+	w.Header().Set("Last-Modified", zf.Modified.UTC().Format(http.TimeFormat))
+
+	fp, err := zf.Open()
+	if err != nil {
+		f.error(w, err, options...)
+		return
+	}
+	defer fp.Close()
+
+	// Sniff the content
+	buf := new(bytes.Buffer)
+	mtype, err := mimetype.DetectReader(io.TeeReader(fp, buf))
+	if err != nil {
+		f.error(w, err, options...)
+		return
+	}
+
+	w.Header().Set("Content-Type", mtype.String())
+
+	ae := r.Header.Get("Accept-Encoding")
+
+	if r.Method == "HEAD" {
+		return
+	}
+
+	// Directly send the compressed data when possible
+	if strings.Contains(ae, "deflate") && zf.Method == zip.Deflate {
+		w.Header().Set("Content-Encoding", "deflate")
+		w.Header().Set("Content-Length", strconv.FormatUint(zf.CompressedSize64, 10))
+
+		cfp, err := zf.OpenRaw()
+		if err != nil {
+			f.error(w, err, options...)
+			return
+		}
+
+		applyOptions(options, w, http.StatusOK)
+		w.WriteHeader(http.StatusOK)
+		if _, err := io.Copy(w, cfp); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Length", strconv.FormatUint(zf.UncompressedSize64, 10))
+	applyOptions(options, w, http.StatusOK)
+	w.WriteHeader(http.StatusOK)
+	_, err = io.Copy(w, io.MultiReader(buf, fp))
+	if err != nil && !errors.Is(err, syscall.EPIPE) {
+		panic(err)
+	}
+}
+
+func (f HTTPZipFile) error(w http.ResponseWriter, err error, options ...serveOption) {
+	status := http.StatusInternalServerError
+	if os.IsNotExist(err) {
+		status = http.StatusNotFound
+	}
+
+	applyOptions(options, w, status)
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (f HTTPZipFile) checkIfModifiedSince(r *http.Request, modtime time.Time) bool {
+	ius := r.Header.Get("If-Modified-Since")
+	if ius == "" {
+		return false
+	}
+	t, err := http.ParseTime(ius)
+	if err != nil {
+		return false
+	}
+
+	modtime = modtime.Truncate(time.Second)
+	if modtime.Before(t) || modtime.Equal(t) {
+		return true
+	}
+	return false
+}
+
+func applyOptions(options []serveOption, w http.ResponseWriter, status int) {
+	for _, f := range options {
+		f(w, status)
+	}
+}
---
+++ pkg/zipfs/http_test.go
@@ -0,0 +1,123 @@
+package zipfs_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"codeberg.org/readeck/readeck/pkg/zipfs"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHttp(t *testing.T) {
+	tests := []struct {
+		path            string
+		headers         map[string]string
+		status          int
+		content         []byte
+		responseHeaders map[string]string
+	}{
+		{
+			"sub",
+			nil,
+			http.StatusNotFound,
+			nil,
+			nil,
+		},
+		{
+			"test-base.txt",
+			nil,
+			http.StatusOK,
+			[]byte("some content that's not compressed\n"),
+			map[string]string{
+				"content-length": "35",
+				"content-type":   "text/plain; charset=utf-8",
+				"last-modified":  "Mon, 18 Sep 2023 18:43:25 GMT",
+			},
+		},
+		{
+			"test-base.txt",
+			map[string]string{
+				"if-modified-since": "Mon, 18 Sep 2023 18:43:25 GMT",
+			},
+			http.StatusNotModified,
+			nil,
+			nil,
+		},
+		{
+			"test-deflate.txt",
+			nil,
+			http.StatusOK,
+			[]byte("some content that is compressed\nsome content that is compressed\nsome content that is compressed\n"),
+			map[string]string{
+				"content-length": "96",
+			},
+		},
+		{
+			"test-deflate.txt",
+			map[string]string{"Accept-Encoding": "deflate"},
+			http.StatusOK,
+			[]byte{0x2b, 0xce, 0xcf, 0x4d, 0x55, 0x48, 0xce, 0xcf, 0x2b, 0x49, 0xcd, 0x2b, 0x51, 0x28, 0xc9, 0x48, 0x2c, 0x51, 0xc8, 0x2c, 0x6, 0xf2, 0x73, 0xb, 0x8a, 0x52, 0x8b, 0x8b, 0x53, 0x53, 0xb8, 0x8a, 0x29, 0x94, 0x7, 0x0},
+			map[string]string{
+				"content-length":   "36",
+				"content-encoding": "deflate",
+			},
+		},
+	}
+
+	srv := zipfs.HTTPZipFile("fixtures/http.zip")
+
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i+1), func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest("GET", test.path, nil)
+			if test.headers != nil {
+				for k, v := range test.headers {
+					r.Header.Set(k, v)
+				}
+			}
+
+			srv.ServeHTTP(w, r)
+			assert.Equal(t, test.status, w.Result().StatusCode)
+			if w.Result().StatusCode != http.StatusOK {
+				return
+			}
+
+			assert.Equal(t, test.content, w.Body.Bytes())
+
+			for k, v := range test.responseHeaders {
+				assert.Equal(t, v, w.Result().Header.Get(k))
+			}
+		})
+	}
+
+	t.Run("method", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("POST", "test-base.txt", nil)
+		srv.ServeHTTP(w, r)
+		assert.Equal(t, http.StatusMethodNotAllowed, w.Result().StatusCode)
+	})
+
+	t.Run("head", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("HEAD", "test-base.txt", nil)
+		srv.ServeHTTP(w, r)
+		assert.Equal(t, http.StatusOK, w.Result().StatusCode)
+		assert.Equal(t, "", w.Body.String())
+	})
+
+	t.Run("zip not found", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("GET", "test-base.txt", nil)
+		zipfs.HTTPZipFile("fixtures/noop.zip").ServeHTTP(w, r)
+		assert.Equal(t, http.StatusNotFound, w.Result().StatusCode)
+	})
+
+	t.Run("corrupt zip", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("GET", "test-base.txt", nil)
+		zipfs.HTTPZipFile("fixtures/corrupt.zip").ServeHTTP(w, r)
+		assert.Equal(t, http.StatusInternalServerError, w.Result().StatusCode)
+	})
+}

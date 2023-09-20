dc325e3868b22160881285f98a5c8064b3cf6892
Olivier Meunier <olivier@neokraft.net>
2023-09-20 11:55:43 +0200

    Merged sidemenu_controller with panel_controller.
    Both are doing almost the same thing and can be one controller with
    one simple change:
    
    add classes "hidden" and "body" (the later for an optional
    body class change on toggle)

--- web/src/controllers/panel_controller.js
+++ web/src/controllers/panel_controller.js
@@ -6,21 +6,28 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["panel", "button"]
+  static classes = ["hidden", "body"]
 
-  initialize() {
-    this.isPanelOn = false
-  }
-
-  toggleMenu(event) {
-    if (this.isPanelOn) {
-      this.panelTarget.classList.add("panel--hidden")
+  toggleMenu() {
+    if (this.isVisible()) {
+      document.body.classList.remove(...this.bodyClasses)
+      this.panelTarget.classList.add(...this.hiddenClasses)
       this.buttonTarget.focus()
     } else {
-      this.panelTarget.classList.remove("panel--hidden")
+      document.body.classList.add(...this.bodyClasses)
+      this.panelTarget.classList.remove(...this.hiddenClasses)
       this.panelTarget.focus()
     }
 
-    this.isPanelOn = !this.isPanelOn
-    this.buttonTarget.setAttribute("aria-expanded", this.isPanelOn)
+    this.buttonTarget.setAttribute("aria-expanded", this.isSidemenuOn)
+  }
+
+  /**
+   * isVisible returns true when the menu/panel is visible
+   *
+   * @returns boolean
+   */
+  isVisible() {
+    return !this.panelTarget.classList.contains(this.hiddenClass)
   }
 }

69af8b6e3b48228ab79392c51c9ca1dd96d1af1c
Olivier Meunier <olivier@neokraft.net>
2023-10-05 06:12:57 +0200

    Forgot all headers tags after h1 in bleach.
    Those were transformed to div tags :/

--- pkg/bleach/consts.go
+++ pkg/bleach/consts.go
@@ -58,6 +58,11 @@ var elementMap = map[string]string{
 	"frame":      "-", // remove
 	"frameset":   "-", // remove
 	"h1":         "",
+	"h2":         "",
+	"h3":         "",
+	"h4":         "",
+	"h5":         "",
+	"h6":         "",
 	"head":       "-", // remove
 	"header":     "",
 	"hgroup":     "",

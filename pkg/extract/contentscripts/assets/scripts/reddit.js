2d13be9dc8100c025c2902121fc97bf097328a24
Olivier Meunier <olivier@neokraft.net>
2024-01-13 12:11:04 +0100

    Improved Reddit content script.
    So far the Reddit content script could only process images and would
    do a poor job for other contents. This updated content script improves
    the situation.
    
    - first, get the requested URL + ".json"
    - fetch the post using the post ID found in the previous response
    - now we're sucking diesel!
    - set the title and author and preview image
    - if there's some content, set it as our HTML content (self posts but not only)
    - if the post is an image, set the bookmark's type to "photo"
    - if it's a gallery, generate an HTML page with all the pictures and
      disable readability
    - if it's a link, add the original link to the html content
    - if it's a video, add the link to the reddit post (we could embed a
      video player with the DASH URL but it needs other changes)
    - we're done!
    
    This might break the day Reddit realizes that this side of their API
    isn't monetized... oh well.

--- pkg/extract/contentscripts/assets/scripts/reddit.js
+++ pkg/extract/contentscripts/assets/scripts/reddit.js
@@ -15,35 +15,93 @@ exports.processMeta = function () {
   let rsp = requests.get(url)
   rsp.raiseForStatus()
   const data = rsp.json()
-  const postID = data[0]?.data?.children[0]?.data?.name
-  const postHint = data[0]?.data?.children[0]?.data?.post_hint
+  const basePost = data[0]?.data?.children[0]?.data
+  if (!basePost) {
+    return
+  }
+  const postID = basePost.name
 
-  if (postHint == "image") {
-    // We have a picture!
+  const postURL = `https://gateway.reddit.com/desktopapi/v1/postcomments/${postID}`
+  rsp = requests.get(postURL)
+  rsp.raiseForStatus()
+  const post = rsp.json().posts?.[postID]
+
+  let html = ""
+
+  // Get the title
+  if (post.title) {
+    $.title = post.title
+  }
+
+  // Get the author
+  if (post.author) {
+    $.author = post.author
+  }
+
+  // Get any content first
+  if (post.media?.content) {
+    html += post.media.content
+  }
+
+  // Check if there is a preview first
+  const preview = findPreview(post)
+  if (preview) {
+    $.meta["x.picture_url"] = preview
+  }
+
+  if (post.media?.type == "image" && preview) {
+    // We have a picture !
+    // Since we fetched the preview first, we only need to set the bookmark type.
     $.type = "photo"
-
-    rsp = requests.get(
-      "https://gateway.reddit.com/desktopapi/v1/postcomments/" + postID,
-    )
-    rsp.raiseForStatus()
-    const post = rsp.json()
-
-    let img = (post.posts[postID]?.media?.resolutions || []).findLast(
-      () => true,
-    )
-    if (img) {
-      $.meta["x.picture_url"] = unescapeURL(img.url)
-    }
-
-    const title = post.posts[postID]?.title
-    if (title) {
-      $.title = title
-    }
-
-    const author = post.posts[postID]?.author
-    if (author) {
-      $.authors = [author]
-    }
     $.description = ""
+    html = ""
+  } else if (post.media?.type == "gallery" && post.media?.gallery?.items) {
+    // Picture gallery. We'll create an HTML content with a link to all images.
+    // They'll be saved during the extraction process
+    const items = post.media.gallery.items
+      .map((item) => {
+        const img = (
+          post.media.mediaMetadata?.[item.mediaId]?.p || []
+        ).findLast(() => true)
+        if (!img) {
+          return ""
+        }
+        return img.u
+      })
+      .filter((src) => src)
+
+    if (items.length > 0) {
+      $.meta["x.picture_url"] = items[0]
+    }
+
+    html += items
+      .map((src) => {
+        return `<figure><img alt="" src="${src}"></figure>`
+      })
+      .join("\n")
+    $.readability = false
+  } else if (post.media?.type == "video") {
+    $.type = "video"
+    html += `<p><a href="${post.permalink}">Original Reddit Video</a></p>`
+  } else if (post.source?.url) {
+    html += `<p>Link to <a href="${post.source.url}">${
+      post.source.displayText || post.source.url
+    }</a></p>`
+  }
+
+  if (html != "") {
+    $.html = `<section id="main">${html}</section>`
+  }
+}
+
+function findPreview(post) {
+  // The last preview is the biggest
+  let img = (post.media?.resolutions || []).findLast(() => true)
+  if (!img) {
+    img = post.preview
+  }
+
+  if (img) {
+    return unescapeURL(img.url)
   }
 }

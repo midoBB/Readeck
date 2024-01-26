89b6b64a638f4676c5cd4bce925f71a74adc2af2
Olivier Meunier <olivier@neokraft.net>
2024-01-26 22:45:37 +0100

    Added colors and forms to the cookbook.
    This was a much needed development feature to test colors and forms
    rendering.

--- internal/cookbook/views.go
+++ internal/cookbook/views.go
@@ -9,6 +9,10 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+
+	"codeberg.org/readeck/readeck/internal/server"
+	"codeberg.org/readeck/readeck/pkg/csp"
+	"codeberg.org/readeck/readeck/pkg/forms"
 )
 
 type cookbookViews struct {
@@ -22,6 +26,8 @@ func newCookbookViews(api *cookbookAPI) *cookbookViews {
 
 	r.With(api.srv.WithPermission("cookbook", "read")).Group(func(r chi.Router) {
 		r.Get("/", v.templateView("prose"))
+		r.Get("/colors", v.templateView("colors"))
+		r.Get("/forms", v.formsView)
 	})
 
 	return v
@@ -30,6 +36,41 @@ func newCookbookViews(api *cookbookAPI) *cookbookViews {
 func (v *cookbookViews) templateView(name string) func(w http.ResponseWriter, r *http.Request) {
 	template := fmt.Sprintf("cookbook/%s", name)
 	return func(w http.ResponseWriter, r *http.Request) {
+		policy := server.GetCSPHeader(r).Clone()
+		policy.Add("style-src-attr", csp.UnsafeInline)
+		policy.Write(w.Header())
+
 		v.srv.RenderTemplate(w, r, 200, template, nil)
 	}
+}
+
+func (v *cookbookViews) formsView(w http.ResponseWriter, r *http.Request) {
+	ctx := server.TC{
+		"Form": newCookbookForm(),
+	}
+
+	v.srv.RenderTemplate(w, r, 200, "cookbook/forms", ctx)
+}
+
+func newCookbookForm() *forms.Form {
+	choices := forms.NewListField("choices", func(s string) forms.Field {
+		return forms.NewTextField(s)
+	}, forms.DefaultListConverter)
+	choices.(*forms.ListField).SetChoices(forms.Choices{
+		{"a", "Choice A"},
+		{"b", "Choice B"},
+		{"c", "Choice C"},
+	})
+
+	f := forms.Must(
+		forms.NewTextField("text"),
+		forms.NewChoiceField("select", forms.Choices{
+			{"", ""},
+			{"choice 1", "Choice 1"},
+			{"choice 2", "Choice 2"},
+		}),
+		choices,
+	)
+
+	return f
 }

9b7fe0be828aa52eed771a69dc664ba6a37a5859
Olivier Meunier <olivier@neokraft.net>
2024-11-05 21:01:48 +0100

    Prevent the sort menu to go offscreen on mobile.
    This adds an "offset" value to the menu controller. Based on that, we
    can check if the menu will be offscreen and set a different position.
    
    Closes #265

--- web/src/controllers/menu_controller.js
+++ web/src/controllers/menu_controller.js
@@ -5,10 +5,19 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = {
+    offset: String,
+  }
+
   connect() {
     if (this.element.tagName.toLowerCase() != "details") {
       return
     }
+
+    this.positionMenu()
+    window.addEventListener("resize", () => {
+      this.positionMenu()
+    })
 
     this.element.addEventListener("toggle", () => {
       if (this.element.open) {
@@ -34,6 +43,34 @@ export default class extends Controller {
     }
     if (!this.element.contains(evt.target)) {
       this.element.open = false
+    }
+  }
+
+  positionMenu = () => {
+    if (!this.offsetValue) {
+      return
+    }
+
+    const el = this.element.querySelector("ul:first-of-type")
+    if (!el) {
+      return
+    }
+    const styleName = this.offsetValue
+    const positionName = styleName == "left" ? "right" : "left"
+
+    if (getComputedStyle(el).position != "absolute") {
+      return
+    }
+
+    // Remove the style first, so the classes have priority
+    el.style[styleName] = ""
+
+    const bound = el.getBoundingClientRect()[positionName]
+    const ww = window.innerWidth
+    if (styleName == "right") {
+      el.style[styleName] = bound < 0 ? `${bound - 4}px` : "0"
+    } else {
+      el.style[styleName] = bound > ww ? `${ww - bound - 4}px` : "0"
     }
   }
 }

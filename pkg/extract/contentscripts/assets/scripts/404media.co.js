d2b8cba9eaa39ab7336de81000e3dec8230fa97f
Olivier Meunier <olivier@neokraft.net>
2024-11-08 08:05:40 +0100

    Added license to 404media content-script

--- pkg/extract/contentscripts/assets/scripts/404media.co.js
+++ pkg/extract/contentscripts/assets/scripts/404media.co.js
@@ -1,7 +1,11 @@
-exports.isActive = function() {
+// SPDX-FileCopyrightText: © 2024 Taniki [https://11d.im/]
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+exports.isActive = function () {
   return $.domain == "404media.co"
 }
 
-exports.processMeta = function(){
-  $.authors = [ $.meta['twitter.data1']  ]
+exports.processMeta = function () {
+  $.authors = [$.meta["twitter.data1"]]
 }

d419aeed76e487f83641feeef3ce977b4636f527
Olivier Meunier <olivier@neokraft.net>
2025-01-02 23:16:39 +0100

    Remove orphan files in cleanup command.
    It should normally never be needed, apart in dev environment where one
    (like me) would make mistakes.

--- internal/app/cleanup.go
+++ internal/app/cleanup.go
@@ -8,7 +8,10 @@ import (
 	"context"
 	"errors"
 	"flag"
+	"fmt"
 	"log/slog"
+	"path/filepath"
+	"strings"
 
 	"github.com/cristalhq/acmd"
 	"github.com/doug-martin/goqu/v9"
@@ -39,6 +42,16 @@ func runCleanup(_ context.Context, args []string) error {
 	}
 	defer appPostRun()
 
+	println("⚙️ removing loading bookmarks")
+	if err := removeLoadingBookmarks(); err != nil {
+		return err
+	}
+
+	println("⚙️ removing orphan files")
+	return removeOrphanFiles()
+}
+
+func removeLoadingBookmarks() error {
 	var items []*bookmarks.Bookmark
 	ds := bookmarks.Bookmarks.Query().Where(
 		goqu.C("state").Eq(bookmarks.StateLoading),
@@ -48,7 +61,7 @@ func runCleanup(_ context.Context, args []string) error {
 	}
 
 	if len(items) == 0 {
-		println("⭐ all good!")
+		println("  ⭐ all good!")
 		return nil
 	}
 
@@ -62,6 +75,45 @@ func runCleanup(_ context.Context, args []string) error {
 			continue
 		}
 		l.Info("bookmark deleted")
+	}
+
+	return nil
+}
+
+func removeOrphanFiles() error {
+	dirs, err := filepath.Glob(filepath.Join(bookmarks.StoragePath(), "*/*.zip"))
+	if err != nil {
+		return err
+	}
+	i := 0
+	for _, x := range dirs {
+		bookmarkID := strings.TrimSuffix(filepath.Base(x), ".zip")
+		_, err := bookmarks.Bookmarks.GetOne(goqu.C("uid").Eq(bookmarkID))
+		if err == nil {
+			continue
+		}
+		if !errors.Is(err, bookmarks.ErrBookmarkNotFound) {
+			return err
+		}
+
+		l := slog.With(
+			slog.String("file", x),
+		)
+		b := &bookmarks.Bookmark{
+			UID: bookmarkID,
+		}
+		u, _ := b.GetBaseFileURL()
+		b.FilePath = u
+		b.RemoveFiles()
+		l.Info("file removed")
+
+		i++
+	}
+
+	if i > 0 {
+		fmt.Printf("  ❌ %d file(s) removed\n", i)
+	} else {
+		println("  ⭐ no orphan files")
 	}
 
 	return nil

f260afd44d91d56991d46e9cfb6f34f2b85cdb9b
Olivier Meunier <olivier@neokraft.net>
2025-03-17 20:01:03 +0100

    Fixed missing licenses in migration 18 files

--- internal/db/migrations/sqlite3/18_auth_last_used.sql
+++ internal/db/migrations/sqlite3/18_auth_last_used.sql
@@ -1,2 +1,6 @@
+-- SPDX-FileCopyrightText: © 2025 Olivier Meunier <olivier@neokraft.net>
+--
+-- SPDX-License-Identifier: AGPL-3.0-only
+
 ALTER TABLE token ADD COLUMN last_used datetime NULL;
 ALTER TABLE credential ADD COLUMN last_used datetime NULL;
